937622c64bb3e293028c0699ab83d5362b657250
Gaurav B Kapoor <gauravbk20@gmail.com>
2026-07-25 18:51:13 -0500

    pythonrelated

---
+++ my_strcmp.py
@@ -0,0 +1,13 @@
+weather="sunny"
+def my_strcmp():
+  wethr = input("The weather:")
+  if wethr.lower() == weather:
+      print("Wow u matched the weather compare variable now go play")
+  elif wethr.lower() == "cloudy":
+      print("the weather is cloudy take your umbrella")
+  elif wethr.lower() == "cold":
+      print("get your coat:")
+  else:
+      print("take it easy and go for a walk if its not raining")
+
+my_strcmp()
