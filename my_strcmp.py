00dc4308aa23045e46fd7f6e8f585eb6be88595f
Gaurav B Kapoor <gauravbk20@gmail.com>
2026-07-25 19:31:18 -0500

    python-related

--- my_strcmp.py
+++ my_strcmp.py
@@ -1,6 +1,8 @@
+import sys
+
 weather="sunny"
 def my_strcmp():
-  wethr = input("The weather:")
+  wethr = sys.argv[1:]
   if wethr.lower() == weather:
       print("Wow u matched the weather compare variable now go play")
   elif wethr.lower() == "cloudy":
